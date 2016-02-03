9379ae0fac47b26ec30213fdf193b91581081fe6
u31949 <u31949@70e30742-fd67-4379-b997-7ee836aa5a11.prvt.dyno.rt.heroku.com>
2016-02-03 03:14:49 +0000

    Initial commit

---
+++ answer.md
@@ -0,0 +1,2 @@
+## Q1. What is your favorite programing language?
+Delete this line and write your favorite programing language in here instead. 
---
+++ app/hello.js
@@ -0,0 +1,10 @@
+function hello(name) {
+/*
+* Implement a function which returns "Hello World!" when "World" is entered in this "name".
+* It would return "Hellow codecheck!" in the case that "codecheck" is entered in this "name".
+*/
+  //implement "Helloworld" in here.
+  //return "Hello World!";
+}
+
+module.exports = hello;
